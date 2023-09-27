7f8e55e417a8a3d94b88183c8ee958b38dd3c0a3
sinans <svrsinan06@gmail.com>
2023-09-27 16:53:21 +0300

    1

---
+++ src/Main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className='container'>
+            <h1 className='text-primary border text-center p-5'> Weather App</h1>
+            <div className="input-group mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Please enter a city name..."
+                    aria-label="Recipient's username"
+                    aria-describedby="button-addon2"
+                />
+                <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                >
+                    Find
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/CityCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CityCard = () => {
+  return (
+    <div>SelectedCity</div>
+  )
+}
+
+export default CityCard;
